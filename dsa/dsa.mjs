b547a2e24b6b351339c4dbb5492e25ca080e08a9
Klusty <makl226@uky.edu>
2024-05-09 13:27:58 -0400

    update

--- dsa/dsa.mjs
+++ dsa/dsa.mjs
@@ -6,7 +6,7 @@ export class DigitalSlideArchiveAPI extends REST{
         let dsa = this;
         this.baseurl = baseurl;
         this.LoginSystem = new LoginSystem(this);
-        this.LoginSystem.autologin();
+        this.LoginSystem.autologin;
 
         dsa.LinkedSVS = function(folderId, name, metadata){
             const formData = new FormData()
